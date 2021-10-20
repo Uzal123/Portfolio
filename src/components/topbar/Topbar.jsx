fa35b60ba3729cc0a87a9b95ea11c7a6ab43c749
Ujjwal-1214 <ujwalpaswan1214@gmail.com>
2021-10-20 22:18:17 +0530

    blank-base

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wapper">
+                <div className="left">
+                    <a href="#intro">Home</a>
+                </div>
+                <div className="right"></div>
+            </div>
+        </div>
+    )
+}
