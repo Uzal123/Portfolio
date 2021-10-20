c703538a97971e9262f396ae80de96e21856b905
Ujjwal-1214 <ujwalpaswan1214@gmail.com>
2021-10-21 02:01:29 +0530

    navbar-intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,23 @@ import "./intro.scss"
 export default function intro() {
     return (
         <div className="intro" id="intro">
-        
+            <div className="left">
+                <div className="imgContainer">
+                <img src="assets/man.png" alt="" srcset="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                <h2>Hi! I am</h2>
+                <h1>Ujjwal Paswan</h1>
+                <h3>and I am a <span>Developer</span></h3>
+                </div>
+                <div>
+                    <a href="#portfolio">
+                        <img src="assets/down.png" alt="" srcset="" />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,29 @@
 import "./topbar.scss"
+import {Person,Email,} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar">
-            <div className="wapper">
+            <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">Home</a>
+                    <a href="#intro" className="logo">Ujjwal.</a>
+                <div className="itemContainer">
+                <Person className="icon"/>
+                <span>+91 8592031812</span>
                 </div>
-                <div className="right"></div>
+                <div className="itemContainer">
+                <Email  className="icon"/>
+                <span>ujwalpaswan1214@gmail.com</span>
+                </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                
+                </div>
             </div>
         </div>
     )
