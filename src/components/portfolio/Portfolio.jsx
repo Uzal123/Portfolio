9a65d4d7912cc1f0308004736051b5c1238d2135
Ujjwal-1214 <ujwalpaswan1214@gmail.com>
2021-10-21 18:43:26 +0530

    portfolio-start

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,39 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <h2>Portfolio</h2>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Recent</li>
+                <li>Websites</li>
+                <li>Design</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                <img src="https://p.kindpng.com/picc/s/215-2154096_creativity-digital-marketing-digital-marketing-creative-png-transparent.png" alt="" srcset="" />
+                <h2>Hotel Website</h2>
+                </div>
+                <div className="item">
+                <img src="https://p.kindpng.com/picc/s/215-2154096_creativity-digital-marketing-digital-marketing-creative-png-transparent.png" alt="" srcset="" />
+                <h2>Hotel Website</h2>
+                </div>
+                <div className="item">
+                <img src="https://p.kindpng.com/picc/s/215-2154096_creativity-digital-marketing-digital-marketing-creative-png-transparent.png" alt="" srcset="" />
+                <h2>Hotel Website</h2>
+                </div>
+                <div className="item">
+                <img src="https://p.kindpng.com/picc/s/215-2154096_creativity-digital-marketing-digital-marketing-creative-png-transparent.png" alt="" srcset="" />
+                <h2>Hotel Website</h2>
+                </div>
+                <div className="item">
+                <img src="https://p.kindpng.com/picc/s/215-2154096_creativity-digital-marketing-digital-marketing-creative-png-transparent.png" alt="" srcset="" />
+                <h2>Hotel Website</h2>
+                </div>
+                <div className="item">
+                <img src="https://p.kindpng.com/picc/s/215-2154096_creativity-digital-marketing-digital-marketing-creative-png-transparent.png" alt="" srcset="" />
+                <h2>Hotel Website</h2>
+                </div>
+            </div> 
         </div>
     )
 }
