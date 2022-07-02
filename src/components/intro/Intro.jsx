180897719ca89078a5a4d70fb505fb86cc1b609c
Ujjwal-1214 <ujwalpaswan1214@gmail.com>
2022-07-02 17:18:31 +0530

    test

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -21,7 +21,7 @@ export default function Intro() {
             <div className="right">
                 <div className="wrapper">
                 <h2>Hi! I am</h2>
-                <h1>Ujjwal Paswan</h1>
+                <h1>Uzwal Paswan</h1>
                 <h3>and I am a <span ref={textRef}></span></h3>
                 </div>
                     <a href="#portfolio">
